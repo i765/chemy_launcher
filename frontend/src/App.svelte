702acfb791c7048215a2034bb8e11ecf8d8731e1
Pavel Vereschagin <ipashu@gmail.com>
2026-09-16 15:47:30 +0300

    sync

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,79 +1,155 @@
 <script lang="ts">
-  import logo from './assets/images/logo-universal.png'
-  import {Greet} from '../wailsjs/go/main/App.js'
+  import { Quit } from "../wailsjs/runtime";
+  import { GetAvailablePorts } from "../wailsjs/go/main/App";
+  import { onMount } from "svelte";
 
-  let resultText: string = "Please enter your name below 👇"
-  let name: string
+  let ports: string[] = [];
+  let selectedPort: string = "";
 
-  function greet(): void {
-    Greet(name).then(result => resultText = result)
+  // Функция для обновления списка портов
+  async function refreshPorts() {
+    try {
+      ports = await GetAvailablePorts();
+      // Если порты есть, а выбpанный порт пустой — выберем первый по умолчанию
+      if (ports.length > 0 && !selectedPort) {
+        selectedPort = ports[0];
+      }
+    } catch (err) {
+      alert("Не удалось получить порты " + err);
+    }
   }
+
+  onMount(() => {
+    refreshPorts();
+  });
 </script>
 
+<!-- ОТКЛЮЧАЕМ СТАНДАРТНОЕ КОНТЕКСТНОЕ МЕНЮ -->
+<svelte:window on:contextmenu={(e) => e.preventDefault()} />
+
 <main>
-  <img alt="Wails logo" id="logo" src="{logo}">
-  <div class="result" id="result">{resultText}</div>
-  <div class="input-box" id="input">
-    <input autocomplete="off" bind:value={name} class="input" id="name" type="text"/>
-    <button class="btn" on:click={greet}>Greet</button>
+  <div class="header">
+    <div class="title">Запускатор</div>
+    <div class="window-controls">
+      <button class="wc-btn" id="minimizeApp" title="Выход" onclick={Quit}
+      ></button>
+    </div>
+  </div>
+  <div class="run-form">
+    <div class="input-box">
+      <label for="port">Порт</label>
+      <select name="port" id="port">
+        {#if ports.length === 0}
+          <option value="">Порты не найдены</option>
+        {:else}
+          {#each ports as port}
+            <option value={port}>{port}</option>
+          {/each}
+        {/if}
+      </select>
+    </div>
+    <div class="input-box">
+      <label for="user">Пользователь</label>
+      <select name="user" id="user">
+        <option value="user1">Оператор 1</option>
+      </select>
+    </div>
+    <div class="input-box">
+      <label for="password">Пароль</label>
+      <input type="password" name="password" id="password" />
+    </div>
+    <div class="spacer"></div>
+    <div class="input-box">
+      <button>Старт</button><button>Имитатор УСО</button><button
+        >Конфигурация</button
+      >
+    </div>
   </div>
 </main>
 
 <style>
-
-  #logo {
-    display: block;
-    width: 50%;
-    height: 50%;
-    margin: auto;
-    padding: 10% 0 0;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
+  main {
+    display: flex;
+    flex-direction: column;
+    user-select: none;
   }
-
-  .result {
+  .run-form {
+    padding: 32px;
+  }
+  .spacer {
     height: 20px;
-    line-height: 20px;
-    margin: 1.5rem auto;
   }
 
-  .input-box .btn {
-    width: 60px;
-    height: 30px;
-    line-height: 30px;
-    border-radius: 3px;
-    border: none;
-    margin: 0 0 0 20px;
-    padding: 0 8px;
-    cursor: pointer;
-  }
+  .input-box {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+    gap: 8px;
 
-  .input-box .btn:hover {
-    background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
-    color: #333333;
-  }
+    select,
+    input,
+    button {
+      font-family: "internal-app-font";
+      font-size: var(--font-size);
+      border-radius: 5px;
+      flex: 1;
+      appearance: none;
+      border: 0;
+      cursor: pointer;
+      color: var(--text-color);
+      background-color: var(--inputs-background-color);
+      padding-left: 16px;
+      height: 38px;
+      border-bottom: 1px solid var(--border-color);
+    }
 
-  .input-box .input {
-    border: none;
-    border-radius: 3px;
-    outline: none;
-    height: 30px;
-    line-height: 30px;
-    padding: 0 10px;
-    background-color: rgba(240, 240, 240, 1);
-    -webkit-font-smoothing: antialiased;
-  }
+    select,
+    input {
+      &:focus {
+        outline: 0;
+        background-color: #252525;
+        color: var(--primary-color);
+      }
+    }
 
-  .input-box .input:hover {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
+    select option {
+      font-size: var(--font-size);
+      background-color: #222222;
+      color: var(--text-color);
+    }
 
-  .input-box .input:focus {
-    border: none;
-    background-color: rgba(255, 255, 255, 1);
-  }
+    label {
+      flex: 1;
+      font-size: var(--font-size);
+      color: var(--text-color);
+      text-align: right;
+    }
 
+    button {
+      padding: 0;
+      height: 40px;
+      color: var(--text-color);
+      background-color: var(--button-background-color);
+
+      box-shadow:
+        0 -1px 1px rgba(255, 255, 255, 0.45),
+        0 4px 4px rgba(0, 0, 0, 0.1),
+        0 19px 15px rgba(255, 255, 255, 0.14) inset;
+
+      border: none;
+      &:hover {
+        background-color: var(--button-background-color-hover);
+        box-shadow: 0 -1px 1px rgba(255, 255, 255, 0.65),
+        0 19px 15px rgba(255, 255, 255, 0.2) inset;
+      }
+      &:active {
+        padding-top: 2px;
+        background-color: var(--button-background-color-active);
+        box-shadow:
+          0 1px 1px rgba(255, 255, 255, 0.25),
+          0 -1px 1px rgba(0, 0, 0, 0.1),
+        0 19px 15px rgba(255, 255, 255, 0.1) inset;
+      }
+    }
+  }
 </style>
--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"go.bug.st/serial"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+// GetAvailablePorts запрашивает порты и возвращает их для фронтенда
+func (a *App) GetAvailablePorts() []string {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Printf("Ошибка получения портов: %v", err)
+		return []string{} // возвращаем пустой массив в случае ошибки
+	}
+	return ports
+}
 
 func main() {
 	// Create an instance of the app structure
@@ -17,13 +29,16 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "chemy_launcher",
-		Width:  1024,
-		Height: 768,
+		Title:         "Launcher",
+		Width:         550,
+		Height:        300,
+		Frameless:     true,
+		AlwaysOnTop:   true,
+		DisableResize: true,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 32, G: 32, B: 32, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
